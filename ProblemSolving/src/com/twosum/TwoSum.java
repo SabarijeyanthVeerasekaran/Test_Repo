b7a4aa3f8bdada54b7ba32a2998f7b440dca116c
Sabarijeyanth V <vsabari487@gmail.com>
2022-11-14 11:07:20 +0530

    First Commit

---
+++ ProblemSolving/src/com/twosum/TwoSum.java
@@ -0,0 +1,31 @@
+package com.twosum;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		
+		int[] nums= {2,7,11,15};
+		int[] result=twoSumSol(nums,9);
+		System.out.println("The index are ");
+		for(int r:result)
+			System.out.println(r);
+		
+
+	}
+
+	
+	public static int[] twoSumSol(int[] nums,int target)
+	{
+		HashMap<Integer,Integer> deltaPair=new HashMap<Integer,Integer>(); 
+		for(int i=0;i<nums.length;i++)
+		{
+			int delta=target-nums[i];
+			if(deltaPair.containsKey(delta))
+				return new int [] {deltaPair.get(delta),i};
+			deltaPair.put(nums[i], i);
+		}
+		return new int[] {-1,-1};
+	}
+}
